83e13ccafd692f0e23a249b818f2c677eb36523d
Kinga060406 <kingapolak20@gmail.com>
2024-03-13 14:27:26 +0100

    dodanie bazy danych

--- EventsReminderApp.MVC/Models/Events.cs
+++ EventsReminderApp.MVC/Models/Events.cs
@@ -2,5 +2,10 @@
 {
     public class Events
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
     }
 }
