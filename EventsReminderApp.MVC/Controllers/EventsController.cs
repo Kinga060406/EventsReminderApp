750a3078c3dccb9fa0fda9df3574237fd1609e01
Kinga060406 <kingapolak20@gmail.com>
2024-03-25 19:57:26 +0100

    get i post

---
+++ EventsReminderApp.MVC/Controllers/EventsController.cs
@@ -0,0 +1,39 @@
+﻿using EventsReminderApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+public class EventsController : Controller
+{
+    private readonly EventsReminderAppContext _context;
+
+    public EventsController(EventsReminderAppContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult AddEventForm()
+    {
+        return PartialView("_AddEventForm");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddEvent(Events events)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Events.Add(events);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", $"An error occurred while saving the event: {ex.Message}");
+            }
+        }
+        return View("Error");
+    }
+}
--- EventsReminderApp.MVC/Controllers/HomeController.cs
+++ EventsReminderApp.MVC/Controllers/HomeController.cs
@@ -29,6 +29,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult AddEvent() 
+    {
+        return View();
+    }
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
