609240ec92e9c55c5857cddf1d247e70f23aa29c
Kinga060406 <kingapolak20@gmail.com>
2024-06-12 18:09:33 +0200

    dodanie walidacji

--- EventsReminderApp.MVC/Models/EventsValidator.cs
+++ EventsReminderApp.MVC/Models/EventsValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using EventsReminderApp.MVC.Models;
+using System;
 
 public class EventsValidator : AbstractValidator<Events>
 {
@@ -10,12 +11,30 @@ public class EventsValidator : AbstractValidator<Events>
             .MaximumLength(100).WithMessage("Name must be less than 100 characters.");
 
         RuleFor(eventss => eventss.Date)
-            .NotNull().WithMessage("Date cannot be null.");
+            .NotNull().WithMessage("Date cannot be null.")
+            .Must(BeAValidDate).WithMessage("Date must be today or in the future.");
 
         RuleFor(eventss => eventss.Time)
-            .NotNull().WithMessage("Time cannot be null.");
+            .NotNull().WithMessage("Time cannot be null.")
+            .Must((events, time) => IsValidTime(events.Date, time))
+            .WithMessage("The time for today must be in the future.");
 
         RuleFor(eventss => eventss.Description)
             .NotNull().WithMessage("Description cannot be null.");
+    }
+
+    private bool BeAValidDate(DateOnly date)
+    {
+        return date >= DateOnly.FromDateTime(DateTime.Now);
+    }
+
+    private bool IsValidTime(DateOnly date, TimeOnly time)
+    {
+        var now = DateTime.Now;
+        if (date == DateOnly.FromDateTime(now))
+        {
+            return time.ToTimeSpan() > now.TimeOfDay;
+        }
+        return true; // If the date is in the future, the time is always valid
     }
 }
